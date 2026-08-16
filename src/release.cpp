cce9e08314eef8039a90367d2b0c23130963153e
karolyia79 <andraskarolyi@hotmail.com>
2026-08-16 07:10:25 +0200

    v1.1.0: API elhagyása

--- src/release.cpp
+++ src/release.cpp
@@ -1,6 +1,6 @@
 #include "release.h"
 
 // Itt tudod könnyen átírni a verziószámot és az egyéb kiadási infókat:
-const char* FW_VERSION   = "v1.0.5";
+const char* FW_VERSION   = "v1.1.0";
 const char* RELEASE_DATE = "2026-08-16";
-const char* FW_BUILD_NAME = "Anderson";
+const char* FW_BUILD_NAME = "Bateman";
